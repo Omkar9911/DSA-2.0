fb61079762640b234546c47d9a590a261e1b70d5
Omkar Jadhav <93418994+Omkar9911@users.noreply.github.com>
2026-05-25 20:26:28 +0530

    Two Sum

---
+++ TwoSum.java
@@ -0,0 +1,25 @@
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target)
+                    return new int[] {i, j};
+            }
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+
+        TwoSum obj = new TwoSum();
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = obj.twoSum(nums, target);
+
+        System.out.println("Output: [" + result[0] + ", " + result[1] + "]");
+    }
+}
